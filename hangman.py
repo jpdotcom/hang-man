6c4a7b4d82cc88c33d2c41b9fe3b71dd531bb0a6
PR0Grammar <npatel3030@gmail.com>
2020-03-30 17:50:07 -0400

    Revert "changed stuff"
    This reverts commit 8218071dc2bbccbe9735d379e9c6d71d267c9dac.

--- hangman.py
+++ hangman.py
@@ -13,20 +13,8 @@ for i in range(0,len(words)):
     b=b[0:len(b)-1]
     words[i]=b
 
-man=open("man.txt")
-
-drawing=[]
-
-for line in man:
-    drawing.append(line)
-
-for i in range(0,len(drawing)):
-    b=drawing[i]
-    b=b[0:len(b)-1]
-    drawing[i]=b
-
 word_to_guess=random.choice(words)
-lives=1
+lives=len(word_to_guess)
 
 def MainFuc():
     print ("Let's play a game of Hangman!. What is your name?")
@@ -44,29 +32,29 @@ def letter_guessing(letter_guess,forming_letter,word_list,letters_said):
     if letter_guess.isalpha()==False:
         print("That's not a letter! I'll reset your turn")
         return
-    if letter_guess[0] in word_list:
-        print(letter_guess[0] + " is in my word!")
+    if letter_guess in word_list:
+        print(letter_guess + " is in my word!")
         for i in range(0,len(word_list)):
-            if word_list[i]==letter_guess[0]:
+            if word_list[i]==letter_guess:
                 forming_letter.pop(i)
-                forming_letter.insert(i,letter_guess[0])
-        letters_said.append(letter_guess[0])
+                forming_letter.insert(i,letter_guess)
+        letters_said.append(letter_guess)
         time.sleep(3)
         print("So this is the word so far", forming_letter)
     if forming_letter== word_list:
         time.sleep(2)
         return True
-    elif letter_guess[0] not in word_list:
-        letters_said.append(letter_guess[0])
+    elif letter_guess not in word_list:
+        letters_said.append(letter_guess)
         lives-=1
-        print("Sorry! " + letter_guess[0] + " is not in my word. You have " + str(lives) + " lives left" )
+        print("Sorry! " + letter_guess + " is not in my word. You have " + str(lives) + " lives left" )
 
 def guess_the_word(word_to_guess):
     global lives
     print("Ok what is your guess:")
     guessed_word=input()
     time.sleep(2)
-    if guessed_word == word_to_guess:
+    if guessed_word==word_to_guess:
         return True
     elif guessed_word!=word_to_guess:
         lives-=1
@@ -88,21 +76,19 @@ def complete_guess_process(word_list):
         print("Want to guess the word or guess a letter")
         word_or_letter=input()
         
-        if "letter" in word_or_letter:
+        if word_or_letter=="letter":
             print("What is your letter")
             letter_guess=input()
             c= letter_guessing(letter_guess,forming_letter,word_list,letters_said)
             if c:
                 return "You've completed the word! It was " + word_to_guess +". You win!"
             
-        elif "word" in word_or_letter:
+        elif word_or_letter == "word":
             b=guess_the_word(word_to_guess)
             if b:
                 return "Correct! You Win"
     if lives==0:
-        for e in drawing:
-            print(e)
-        return "You lost you pathetic subhuman filth. You are not worthy of exisitng and should be hanged. This was my word: " + word_to_guess
+        return "You lost you pathetic subhuman filth. You are not worthy of exisitng and should be hanged.:) This was my word ", word_to_guess
             
             
 print(word_to_guess)
